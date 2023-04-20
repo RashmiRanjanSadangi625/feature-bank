fc476334b76fb6637203dce7467dad444590a081
RashmiRanjanSadangi625 <sadangirashmiranjan@gmail.com>
2023-04-20 00:13:36 -0700

    Added new files

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,9 @@
 const mongoose =require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/featuredb",
+mongoose.connect("mongodb://127.0.0.1:27017/featuredb",
 	{
 		useNewUrlParser : true,
 		useUnifiedTopology:true
 	})
 .then(()=>{console.log("Connection Succesfull...!");})
 .catch((e)=> {console.log("Not connected with db");})
-
